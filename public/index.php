82bba367171c75d7b0fddf1b2fce22e91f27ef7d
UpLink <dragone85@gmx.ch>
2017-03-30 01:00:11 +0200

    Added Database connection model

--- public/index.php
+++ public/index.php
@@ -1,10 +1,24 @@
 <?php
 
+include __DIR__ . '/model/DBAccess.php';
 
-if (!empty($_POST['loginUsername'])){
-    include __DIR__ . '/Main/home.html';
-}else{
-    include __DIR__.'/Login/login.html';
+session_start();
+
+$siteAction = '';
+
+$siteAction = (isset($_GET['siteAction'])) ? $_GET['siteAction'] : '';
+
+var_dump($_SESSION);
+
+$model = new DBAccess();
+
+switch ($siteAction) {
+    case 'login':
+        include __DIR__ . '/Login/login.html';
+        break;
+    default:
+        include __DIR__ . '/Main/home.html';
+        break;
 }
 
 
---
+++ public/model/DBAccess.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Stefano Zizza
+ * Date: 30.03.17
+ * Time: 00:35
+ */
+class DBAccess
+{
+    /**
+     * @var null
+     */
+    private $db = null;
+
+    function __construct()
+    {
+        $dsn = 'mysql:host=127.0.0.1;dbname=cms';
+        $user = 'root';
+        $password = 'root';
+
+
+        $this->db = new PDO($dsn,$user,$password);
+
+        $this->test();
+    }
+
+    private function test()
+    {
+        $result = $this->db->prepare('SELECT * FROM cms.login');
+        $result->execute();
+        echo '<pre>';
+        var_dump($result->fetchAll());
+        echo '</pre>';
+    }
+}
